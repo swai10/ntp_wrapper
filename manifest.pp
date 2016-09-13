9c8d4f87ea40120a2112ff027c69c367fac52c45
root <root@clemence.puppetlabs.vm>
2016-09-12 20:31:23 -0700

    Another commit

--- manifest.pp
+++ manifest.pp
@@ -1,20 +1,23 @@
-class ntp_wrapper {
+class ntp_wrapper (
+  $server1 = '0.pool.ntp.org'
+  $server2 = '1.pool.mtp.org',
+)  {
     package { 'ntp':
       ensure   => present,
      
-  } 
-}
+  }
+
 file { 'etc/ntp.conf':
      ensure  => file,
      content => 'root',
      mode    => '0644',
      owner   => 'root',
-     source  => 'puppet:///modukes/ntp_wrapper/ntp.conf',
+     content => epp('ntp_wrapper/ntp.conf.epp'),
      require => Package['ntp'],
      notify  => Service['ntpd'],
   }
 service { 'ntpd':
     ensure    => running,
     enable    => true,
-    }  
+  }  
 }
